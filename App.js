3118b5cc212dbdf03093d38a70401fda42f4fe70
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-12-12 21:27:39 +0200

    init

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hi world</Text>
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'tomato',
     alignItems: 'center',
     justifyContent: 'center',
   },
